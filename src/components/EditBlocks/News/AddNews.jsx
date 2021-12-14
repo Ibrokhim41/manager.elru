d68904ffbbe5107177925129c8cfcfb8d3f8592c
Ibrokhim41 <ibrokhim.abdurakhmonov@gmail.com>
2021-12-14 11:11:20 +0500

    edit news

--- src/components/EditBlocks/News/AddNews.jsx
+++ src/components/EditBlocks/News/AddNews.jsx
@@ -1,9 +1,29 @@
 import { VscChevronLeft } from "react-icons/vsc";
 import { IoMdImages } from "react-icons/io";
 import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
 const AddNews = () => {
   const route = useHistory();
+
+  const [newsDetail, setNewsDetail] = useState({
+    "id": null,
+    "image": "http://127.0.0.1:8000/media/image/Screenshot_from_2021-12-13_07-33-49.png",
+    "image1": null,
+    "image2": "https://media.istockphoto.com/photos/row-of-books-on-a-shelf-multicolored-book-spines-stack-in-the-picture-id1222550815",
+    "title_uz": null,
+    "title_ru": null,
+    "title_en": null,
+    "info_uz": null,
+    "info_ru": null,
+    "info_en": null,
+    "content_uz": null,
+    "content_ru": null,
+    "content_en": null,
+    "created_at": null
+  })
+
+
 
   return (
     <div className="container mx-auto mb-10 overflow-x-scroll hide-scroll">
@@ -17,6 +37,7 @@ const AddNews = () => {
         <VscChevronLeft className="text-2xl" /> Назад
       </div>
 
+      {/* ru */}
       {/* title */}
       <div className="text-grey-dark ctext-lg font-medium mt-6 mb-5 flex flex-col sm:flex-row sm:justify-between">
         Добавить статью
@@ -25,7 +46,7 @@ const AddNews = () => {
       {/*  title */}
       <div className="flex flex-col md:justify-start mt-4">
         <label htmlFor="category_name" className="text-grey-dark ctext-base ">
-          Заголовок 1
+          Заголовок <span className={`text-blue text-sm`}>ru</span>
         </label>
         <input
           id="category_name"
@@ -38,7 +59,7 @@ const AddNews = () => {
       {/*  text */}
       <div className="flex flex-col md:justify-start mt-4">
         <label htmlFor="category_name" className="text-grey-dark ctext-base ">
-          Абзац 1
+          Текст <span className={`text-blue text-sm`}>ru</span>
         </label>
         <textarea
           id="category_name"
@@ -49,11 +70,11 @@ const AddNews = () => {
         />
       </div>
 
-      {/* 2 */}
+      {/* uz */}
       {/*  title */}
       <div className="flex flex-col md:justify-start mt-6">
         <label htmlFor="category_name" className="text-grey-dark ctext-base ">
-          Заголовок 2
+          Sarlavha <span className={`text-blue text-sm`}>uz</span>
         </label>
         <input
           id="category_name"
@@ -66,7 +87,35 @@ const AddNews = () => {
       {/*  text */}
       <div className="flex flex-col md:justify-start mt-4">
         <label htmlFor="category_name" className="text-grey-dark ctext-base ">
-          Абзац 2
+          Matn <span className={`text-blue text-sm`}>uz</span>
+        </label>
+        <textarea
+          id="category_name"
+          rows="4"
+          type="text"
+          placeholder="Введите заголовок..."
+          className="text-grey-dark ctext-xs w-full md:w-9/12 bg-white border border-grey-border rounded-md focus:outline-none py-2 px-4 mt-1"
+        />
+      </div>
+
+      {/* en */}
+      {/*  title */}
+      <div className="flex flex-col md:justify-start mt-6">
+        <label htmlFor="category_name" className="text-grey-dark ctext-base ">
+          Title <span className={`text-blue text-sm`}>en</span>
+        </label>
+        <input
+          id="category_name"
+          type="text"
+          placeholder="Введите заголовок..."
+          className="text-grey-dark ctext-xs w-full md:w-9/12 bg-white border border-grey-border rounded-md focus:outline-none py-2 px-4 mt-1"
+        />
+      </div>
+
+      {/*  text */}
+      <div className="flex flex-col md:justify-start mt-4">
+        <label htmlFor="category_name" className="text-grey-dark ctext-base ">
+          Text <span className={`text-blue text-sm`}>en</span>
         </label>
         <textarea
           id="category_name"
@@ -81,12 +130,16 @@ const AddNews = () => {
       <div className="flex flex-wrap">
         <div className="w-64 mt-4">
           <label htmlFor="book_image3">
-            <div className="cursor-pointer border border-grey-border bg-white rounded-md w-60 h-28 flex flex-col justify-center items-center">
-              <IoMdImages className="text-4xl text-blue" />
-              <div className="text-grey-dark ctext-base">Загрузить фото</div>
+            <div
+              style={{ backgroundImage: `url(${newsDetail.image})` }}
+              className={`bg-cover bg-center cursor-pointer border border-grey-border bg-white rounded-md w-60 h-28 flex flex-col justify-center items-center`}>
+              {newsDetail.image === null && <IoMdImages className="text-4xl text-blue" />}
+              <div className={`${newsDetail.image !== null && 'hidden'} text-grey-dark ctext-base`}>Загрузить фото</div>
             </div>
           </label>
-          <input type="file" id="book_image3" className="hidden" />
+          <input
+            onChange={(e) => setNewsDetail({ ...newsDetail, image: URL.createObjectURL(e.target.files[0]) })}
+            type="file" id="book_image3" className="hidden" />
           <div className="text-grey-dark ctext-base mt-1 font-bold">
             Фото обложки стати
           </div>
@@ -96,34 +149,42 @@ const AddNews = () => {
         </div>
         {/* 2 */}
         <div className="w-64 mt-4">
-          <label htmlFor="book_image3">
-            <div className="cursor-pointer border border-grey-border bg-white rounded-md w-60 h-28 flex flex-col justify-center items-center">
-              <IoMdImages className="text-4xl text-blue" />
-              <div className="text-grey-dark ctext-base">Загрузить фото</div>
+          <label htmlFor="book_image4">
+            <div
+              style={{ backgroundImage: `url(${newsDetail.image1})` }}
+              className={`bg-cover bg-center cursor-pointer border border-grey-border bg-white rounded-md w-60 h-28 flex flex-col justify-center items-center`}>
+              {newsDetail.image1 === null && <IoMdImages className="text-4xl text-blue" />}
+              <div className={`${newsDetail.image1 !== null && 'hidden'} text-grey-dark ctext-base`}>Загрузить фото</div>
             </div>
           </label>
-          <input type="file" id="book_image3" className="hidden" />
+          <input
+            onChange={(e) => setNewsDetail({ ...newsDetail, image1: URL.createObjectURL(e.target.files[0]) })}
+            type="file" id="book_image4" className="hidden" />
           <div className="text-grey-dark ctext-base mt-1 font-bold">
-          Первое фото статьи
+            Первое фото статьи
           </div>
           <div className="text-grey-dark ctext-base mt-1 font-medium">
-          Минимальный размер: 777х544
+            Минимальный размер: 777х544
           </div>
         </div>
         {/* 3 */}
         <div className="w-64 mt-4">
-          <label htmlFor="book_image3">
-            <div className="cursor-pointer border border-grey-border bg-white rounded-md w-60 h-28 flex flex-col justify-center items-center">
-              <IoMdImages className="text-4xl text-blue" />
-              <div className="text-grey-dark ctext-base">Загрузить фото</div>
+          <label htmlFor="book_image5">
+            <div
+              style={{ backgroundImage: `url(${newsDetail.image2})` }}
+              className={`bg-cover bg-center cursor-pointer border border-grey-border bg-white rounded-md w-60 h-28 flex flex-col justify-center items-center`}>
+              {newsDetail.image2 === null && <IoMdImages className="text-4xl text-blue" />}
+              <div className={`${newsDetail.image2 !== null && 'hidden'} text-grey-dark ctext-base`}>Загрузить фото</div>
             </div>
           </label>
-          <input type="file" id="book_image3" className="hidden" />
+          <input 
+            onChange={(e) => setNewsDetail({ ...newsDetail, image2: URL.createObjectURL(e.target.files[0]) })}
+            type="file" id="book_image5" className="hidden" />
           <div className="text-grey-dark ctext-base mt-1 font-bold">
-          Второе фото статьи
+            Второе фото статьи
           </div>
           <div className="text-grey-dark ctext-base mt-1 font-medium">
-          Минимальный размер: 777х544
+            Минимальный размер: 777х544
           </div>
         </div>
       </div>
